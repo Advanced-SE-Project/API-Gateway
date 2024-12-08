0c56a5c23e92cc43944e3911eb98e188b1bddb7c
niwiunig <niwiunig@edu.aau.at>
2024-12-08 21:50:25 +0100

    added proxying for auth and added validation of token in gateway

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const transactionServiceRouter = require('./src/routes/transaction');
+const authServiceRouter = require('./src/routes/auth');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./src/swagger/swaggerConfig');
+const axios = require('axios');
+
 dotenv.config();
 
-const port = process.env.PORT;
+const PORT = process.env.PORT;
+const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://localhost:5000';
+
 const app = express();
 
+// Set up Swagger UI at the `/swagger` endpoint
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Use json middleware only for non-proxied routes
-app.use((req, res, next) => {
-    if (!req.url.startsWith('/transaction-service')) {
-        express.json()(req, res, next);
-    } else {
-        next();
+// Middleware for validating JWT
+app.use(async (req, res, next) => {
+    // Skip JWT validation for unprotected routes like register and login
+    if (req.url.startsWith('/api/auth/register') || req.url.startsWith('/api/auth/login')) { //ToDo: Move into Method
+        return next();
+    } 
+
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authorization token is missing' });
+    }
+
+    try {
+        const response = await axios.post(`${AUTH_SERVICE_URL}/api/auth/validate`, {}, { headers: { 'authorization': req.headers['authorization'] } });
+
+        if (response.status === 200 && response.data.valid) {
+            console.log('JWT validated successfully');
+            req.user = response.data.user;
+            next();
+        } else {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    } catch (err) {
+        console.error('Error during JWT validation:', err);
+        return res.status(500).json({ message: 'Failed to validate token' });
     }
 });
 
@@ -24,10 +50,14 @@ app.use((req, res, next) => {
     next();
 });
 
-// Proxy routes
+// Auth routes
+app.use('/auth-service', authServiceRouter);
+
+// Transaction routes
 app.use('/transaction-service', transactionServiceRouter);
 
-app.listen(port, () => {
-    console.log(`API Gateway Listening at http://localhost:${port}`);
+// Start the server and listen on the configured port
+app.listen(PORT, () => {
+    console.log(`API Gateway Listening at http://localhost:${PORT}`);
     console.log("Swagger Docs available at http://localhost:4000/swagger");
 });
