28ce48dc6a74511587966896392e4d6f6c70356c
niwiunig <niwiunig@edu.aau.at>
2025-01-19 23:34:09 +0100

    added analytics router

---
+++ src/routes/analytics.ts
@@ -0,0 +1,36 @@
+import express, { Router } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const router: Router = express.Router();
+
+// Middleware to log incoming requests to the gateway
+router.use((req, res, next) => {
+    console.log('Incoming Request to Gateway:', {
+        method: req.method,
+        url: req.url,
+        body: req.body,
+    });
+    next();
+});
+
+// Middleware to log outgoing responses from the gateway
+router.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log('Response from Gateway:', res.statusCode);
+    });
+    next();
+});
+
+router.use(
+    '/',
+    createProxyMiddleware({
+        target: process.env.ANALYTICS_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: { '^/analytics-service': '' }, // Remove the "/transaction-service" prefix before forwarding
+    })
+);
+
+export default router;
